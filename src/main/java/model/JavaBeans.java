51345e08f4ec4390aab49aed74952d32120361fd
danilosenati <danilosena.ti@gmail.com>
2025-06-28 00:11:31 -0300

    feat: add class JavaBeans

--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -1,5 +1,61 @@
 package model;
 
 public class JavaBeans {
+	private int idContato;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	// CONSTRUTOR PADRÃO
+	public JavaBeans() {
+		super();
+	}
+	
+	// CONSTRUTOR COM PARAMETRO
+	
+	public JavaBeans(int idContato, String nome, String telefone, String email) {
+		super();
+		this.idContato = idContato;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	
+	
+	public int getIdContato() {
+		return idContato;
+	}
+	
 
+	public void setIdContato(int id) {
+		this.idContato = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
 }
