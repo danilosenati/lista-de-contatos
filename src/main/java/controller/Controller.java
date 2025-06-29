889e30fcbb0422c505aeab78766ef2d041c12c4a
danilosenati <danilosena.ti@gmail.com>
2025-06-28 23:07:48 -0300

    feat add files

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,33 +8,49 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import model.DAO; // IMPORTANTO CLASSE DAO PARA EXECUTAR O TESTE DE CONEXAO.
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main" }) // AS REQUISIÇÕES SÃO RECEBIDAS ATRAVÉS DE URLs (CAMINHOS
+														// DEFINIDOS EM FORMULARIOS, BOTÕES ETC), CONFIGURADAS NESTA
+														// LINHA.
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	// INSTANCIANDO E CRIANDO UM OBJETO PARA REALIZAR O TESTE DE CONEXÃO:
-	/* DAO conexaoDAO = new DAO(); */  
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		// TESTANDO A CONEXÃO COM O DAO:
-			/* conexaoDAO.testeConexao(); */
+	// INSTANCIANDO E CRIANDO UM OBJETO PARA REALIZAR O TESTE DE CONEXÃO:
+	DAO conexaoDAO = new DAO();
+
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// metodo principal do servlet:
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// conexaoDAO.testeConexao(); TESTANDO A CONEXÃO COM O DAO.
+
+		/*
+		 * REDIRECIONANDO AS REQUISIÇÕES CRIADAS NA LINHA 11 PARA METODOS ESPECIFICOS:
+		 */
+		// String action; VARIAVEL QUE ARMAZENARÁ O CAMINHO DA REQUISIÇÃO.
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) { // SE O CONTEUDO DE ACTION FOR IGUAL A /MAIN
+
+			// SE O METODO TOGET RECEBER A REQUISIÇÃO /MAIN, REDIRECIONE PARA O MÉTODO QUE
+			// VAI TRABALHAR COM ESSA REQUISIÇÃO:
+			contatos(request, response);
+
+		}
+
+	}
+
+	// LISTAR CONTATOS:
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// REDIRECIONAR A REQUISIÇÃO AO DOCUMENTO AGENDA.JSP:
+		response.sendRedirect("Agenda.jsp");
 	}
 
 }
